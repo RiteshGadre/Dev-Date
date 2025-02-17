dac2f90a37954fdbe41caf40a0da8b492ffe37e0
RiteshGadre <riteshgadre9@gmail.com>
2025-02-17 13:59:29 +0530

    HTTP request trial

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("Hello")
-    
+app.get("/user", (req, res) => {
+    res.send("Data sent from the server");
+})
+
+app.post("/user", (req, res) => {
+    res.send("Data sent to the server");
+})
+
+app.put("/user", (req, res) => {
+    res.send("Data updated on the server");
+})
+
+app.patch("/user", (req, res) => {
+    res.send("Data small portion uodated on the server");
+})
+
+app.delete("/user", (req, res) => {
+    res.send("Data deleted from the server");
 })
 
 app.listen(3000, () => {
