5389865d8a9332b93304fb3e24a3837ec4e2b310
RiteshGadre <riteshgadre9@gmail.com>
2025-02-17 14:57:46 +0530

    multiple request handlers practice

--- src/app.js
+++ src/app.js
@@ -1,25 +1,30 @@
 const express = require('express');
 const app = express();
 
-app.get("/user", (req, res) => {
-    res.send("Data sent from the server");
+app.use("/user", (req, res, next) => {
+    console.log("Handliing Router1");
+    
+    // res.send("Data sent from the server");
+    next();
+},
+(req, res, next)=>{
+    console.log("Handliing Router2");
+    next();
+},
+(req, res, next)=>{
+    console.log("Handliing Router3");
+    next();
+},
+(req, res, next)=>{
+    console.log("Handliing Router4");
+    next();
+},
+(req, res, next)=>{
+    console.log("Handliing Router 5");
+    res.send("Handler 5 hadled the request")
 })
 
-app.post("/user", (req, res) => {
-    res.send("Data sent to the server");
-})
 
-app.put("/user", (req, res) => {
-    res.send("Data updated on the server");
-})
-
-app.patch("/user", (req, res) => {
-    res.send("Data small portion uodated on the server");
-})
-
-app.delete("/user", (req, res) => {
-    res.send("Data deleted from the server");
-})
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
